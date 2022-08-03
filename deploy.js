fb3bd3848e454aaa5ec7dcb081f2df7f9bfed4f2
Ben <binhduongquangvtca@gmail.com>
2022-08-04 00:26:29 +0700

    update

--- deploy.js
+++ deploy.js
@@ -1,31 +1,35 @@
-const ethers = require("ethers");
-const fs = require("fs-extra");
-require("dotenv").config();
+const ethers = require("ethers")
+const fs = require("fs-extra")
+require("dotenv").config()
 
-const aibFilePath = "contacts_SimpleStorage_sol_SimpleStorage.abi";
-const binFilePath = "contacts_SimpleStorage_sol_SimpleStorage.bin";
+const aibFilePath = "contacts_SimpleStorage_sol_SimpleStorage.abi"
+const binFilePath = "contacts_SimpleStorage_sol_SimpleStorage.bin"
 
 async function main() {
-  const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
+  const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL)
 
-  // TODO: Old wallet code.
+  // ! Old wallet code.
   // const wallet = new ethers.Wallet(process.env.PRIVATE_KEY);
-  const encryptedJsonKey = fs.readFileSync("./encryptedJsonKey.json", "utf8");
+
+  const encryptedJsonKey = fs.readFileSync("./encryptedJsonKey.json", "utf8")
   const wallet = await new ethers.Wallet.fromEncryptedJsonSync(
     encryptedJsonKey,
     process.env.PRIVATE_KEY_PASSWORD
-  ).connect(provider);
+  ).connect(provider)
 
-  const binary = fs.readFileSync(binFilePath, "utf8");
-  const abi = fs.readFileSync(aibFilePath, "utf8");
-  const { chainId } = await provider.getNetwork();
+  const binary = fs.readFileSync(binFilePath, "utf8")
+  const abi = fs.readFileSync(aibFilePath, "utf8")
+  const { chainId } = await provider.getNetwork()
 
   // TODO: send transaction factory
-  const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
-  const contract = await contractFactory.deploy();
-  const transactionRespone = await contract.store("7");
-  const currentFavoriteNumber = await contract.retrieve();
-  console.log("gasPrice", transactionRespone.gasPrice.toString());
+  const contractFactory = new ethers.ContractFactory(abi, binary, wallet)
+  const contract = await contractFactory.deploy()
+  await contract.deployTransaction.wait(1)
+  let currentFavoriteNumber = await contract.retrieve()
+  const transactionRespone = await contract.store("7")
+  currentFavoriteNumber = await contract.retrieve()
+  console.log("address", contract.address)
+  console.log(`New Favorite Number: ${currentFavoriteNumber}`)
 
   // ! deploy with only transaction data !
   // TODO: send transaction
@@ -46,6 +50,6 @@ async function main() {
 main()
   .then(() => process.exit(0))
   .catch((err) => {
-    console.error(err);
-    process.exit(1);
-  });
+    console.error(err)
+    process.exit(1)
+  })
